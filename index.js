4aca07852b0cde66177445db1ddfc9dd9e59c653
unknown <lipikapanda88@gmail.com>
2020-01-14 18:04:35 +0530

    ok
    Signed-off-by: unknown <lipikapanda88@gmail.com>

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ restService.post("/echo", function(req, res) {
     req.body.queryResult.parameters &&
     req.body.queryResult.parameters.echoText
       ? req.body.queryResult.parameters.echoText
-      : "Seems like some problem. Speak again.";
+      : "Seems like some problem. Speak again please.";
   
   var speechResponse = {
     google: {
-      expectUserResponse: false,
+      expectUserResponse: true,
       richResponse: {
         items: [
           {
